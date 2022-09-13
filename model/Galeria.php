f94c0dc5a1abfc1761f4bbf0656f8e9e556284d1
Carlos Daniel <cdg4305@gmail.com>
2022-09-12 22:12:50 -0300

    Implemntação de dados na galeria controller e model

--- model/Galeria.php
+++ model/Galeria.php
@@ -24,3 +24,39 @@ class Galeria extends Banco{
     public function setFoto($foto){
         $this->foto = $foto;
     }
+
+    public function save() {
+        $result = false;
+        //cria um objeto do tipo conexao
+        $conexao = new Conexao();
+        //cria a conexao com o banco de dados
+        if($conn = $conexao->getConection()){
+            if($this->id > 0){
+                //cria query de update passando os atributos que serão atualizados
+                $query = "UPDATE imovel SET descricao = :descricao, foto = :foto, valor = :valor, tipo = :tipo, fotoTipo = :fotoTipo
+                            path = :path WHERE id = :id";
+                //Prepara a query para execução
+                $stmt = $conn->prepare($query);
+                //executa a query
+                if ($stmt->execute(
+                    array(':descricao' => $this->descricao, ':foto' => $this->foto, ':valor' => $this->valor,':tipo' => $this->tipo, ':id'=> $this->id, 
+                    ':fotoTipo' => $this->fotoTipo, ':path' => $this ->path))){
+                    $result = $stmt->rowCount();
+                }
+            }else{
+                //cria query de inserção passando os atributos que serão armazenados
+                $query = "insert into imovel (id, descricao, foto, valor, tipo, fotoTipo, path) 
+                values (null,:descricao,:foto,:valor,:tipo,:fotoTipo,:path)";
+                //Prepara a query para execução
+                $stmt = $conn->prepare($query);
+                //executa a query
+                if ($stmt->execute(array(':descricao' => $this->descricao, ':foto' => $this->foto, ':valor' => $this->valor,
+                ':tipo' => $this->tipo, ':fotoTipo'=>$this->fotoTipo, ':path' => $this->path))) {
+                    $result = $stmt->rowCount();
+                }
+            }
+        }
+        return $result;
+    }
+    
+    
