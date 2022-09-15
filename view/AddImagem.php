fc867382feda9b3e3793cbac875730bbd8e63364
Carlos Daniel <cdg4305@gmail.com>
2022-09-14 22:42:15 -0300

    Implementação no css do AddImagem e mudanças na view AddImagem

--- view/AddImagem.php
+++ view/AddImagem.php
@@ -2,24 +2,73 @@
     require_once 'controller/GaleriaController.php';
     require_once 'header.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro Galeria</title>
-</head>
-<body>
-<div class="cadastro">
-        <form name="cadImovel" id="cadImovel" action="" method="post" enctype="multipart/form-data">
+
+<link rel="stylesheet" href="wwwroot/css/addImagem.css"> 
+
+    <div class="cadastro">
+        <form name="addImagem" id="addImagem" action="" method="post" enctype="multipart/form-data">
             <div class="">
-                <label for="foto">Foto da Galeria: </label>  
-                <input type="file" name="fotoG" id="fotoG">
-                <input type="submit" value="Salvar" style="width: 80px;">                
+                <label for="foto">Galeria: </label>  
+                <input type="file" name="foto" id="foto"
+                value="<?php echo isset($galeria)?$galeria->getFoto():''; ?>"/><br>
+                <?php
+                    if(isset($galeria) && !empty($galeria->getFoto())){
+                ?>
+                <div class="forn-group form-row">
+                    <div class="text-center">
+                        <img class="img-thumbnail" style="width: 25%;"
+                        src="<?php echo $galeria->getFotoTipo();?>;base64,<?php echo base64_encode($galeria->getFoto());?><?php echo $galeria->getPath();?>">
+                    </div>
+                </div>
+                <?php
+                    }
+                ?>
+                <input type="hidden" name="id" id="id" value="<?php echo isset($galeria)?$galeria->getId():''; ?>" />
+                <input type="hidden" name="path" id="path" value="<?php echo isset($galeria)?$galeria->getPath():''; ?>" />
+                <button name="btnSalvar" id="btnSalvar" value="salvar">Salvar</button>                            
+        </form>        
+                <?php
+                    $galerias = call_user_func(array('GaleriaController', 'listar'));
+                    //Verifica se houve algum retorno
+                    if(isset($galerias) && !empty($galerias)){
+                        foreach($galerias as $galerias){
+                ?> 
+                <tr>                    
+                    <!-- Como o retorno é um objeto, devemos chamar os gets para mostrar o resultado -->
+                    <td><?php echo $galeria->getIdImovel();?></td>
+                    <td><img class="img-thumbnail" style="width: 25%;"
+                    src="data:<?php echo $galeria->getFotoTipo();?>;base64,<?php echo base64_encode($galeria->getFoto());?>"></td>
+                    <td>
+                        <button><a href="index.php?page=imovel&action=excluir&id=<?php echo $imovel->getId(); ?>">Excluir</a></button>                      
+                    </td>
+                </tr>
+                <?php                
+                        }
+                    }else{
+                        ?>
+                        <tr>
+                            <td colspan="3">Nenhum registro cadastrado</td>
+                        </tr>
+                        <?php
+                    }
+                    ?>
             </div>                                
-        </form>
     </div>
+
+    <?php
+
+//Verifica se o botão submit foi acionado
+if(isset($_POST['btnSalvar'])){
+
+    //Chama uma função PHP que permite informar a classe e o Método que será acionado
+    if(isset($imovel)){
+        call_user_func(array('GaleriaController','salvar'),$galeria->getFoto(),$galeria->getFotoTipo());
+    }else{
+        call_user_func(array('GaleriaController','salvar'));
+    }
     
-</body>
-</html>
+    header('Location: index.php?action=listar');
+}
+
+?>
+    
